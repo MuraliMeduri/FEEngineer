662a44c5168049d8eb5bd4a07126f7c87ac8b68a
muralimeduri <meduri1@gmail.com>
2021-02-18 16:40:16 -0600

    FEE assignment

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalNavigation from "../ReusableComponents/GlobalNavigation";
+
+function HomePage() {
+  return (
+    <div>
+      <GlobalNavigation />
+    </div>
+  );
+}
+
+export default HomePage;
